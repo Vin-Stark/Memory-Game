32e4ac8a173ae506dd58ddf590c110ce0da91364
Vin-Stark <77569887+Vin-Stark@users.noreply.github.com>
2023-04-16 16:42:56 +0530

    Update admin.php

--- admin.php
+++ admin.php
@@ -83,7 +83,7 @@
     <!-- Custom styles for this template -->
     <!-- <link href="signin.css" rel="stylesheet"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-  <link rel="stylesheet" href="styles2.css">
+  <link rel="stylesheet" href="style.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@500&display=swap" rel="stylesheet">
